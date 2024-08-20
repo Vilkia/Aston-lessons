922aae49f10f868ab56407d03343175d4ab199a6
Vilka <konstantsin.maliuk@gmail.com>
2024-08-20 21:14:46 +0300

    Исправления согласно задания

--- Lesson-6/src/Main.java
+++ Lesson-6/src/Main.java
@@ -1,17 +1,17 @@
 public class Main {
     public static void main(String[] args) {
         // Создаем массив сотрудников
-        Employee[] employees = new Employee[5];
+        Сотрудник[] employees = new Сотрудник[5];
 
         // Заполняем массив объектами
-        employees[0] = new Employee("Ivan Ivanov", "Engineer", "ivanov@mailbox.com", "892312312", 30000, 30);
-        employees[1] = new Employee("Petr Petrov", "Manager", "petrov@mailbox.com", "892345678", 35000, 28);
-        employees[2] = new Employee("Sidor Sidorov", "Developer", "sidorov@mailbox.com", "892376543", 45000, 32);
-        employees[3] = new Employee("Kuzma Kuznetsov", "Analyst", "kuznetsov@mailbox.com", "892398765", 40000, 29);
-        employees[4] = new Employee("Vasily Vasiliev", "Director", "vasiliev@mailbox.com", "892312398", 50000, 45);
+        employees[0] = new Сотрудник("Ivan Ivanov", "Engineer", "ivanov@mailbox.com", "892312312", 30000, 30);
+        employees[1] = new Сотрудник("Petr Petrov", "Manager", "petrov@mailbox.com", "892345678", 35000, 28);
+        employees[2] = new Сотрудник("Sidor Sidorov", "Developer", "sidorov@mailbox.com", "892376543", 45000, 32);
+        employees[3] = new Сотрудник("Kuzma Kuznetsov", "Analyst", "kuznetsov@mailbox.com", "892398765", 40000, 29);
+        employees[4] = new Сотрудник("Vasily Vasiliev", "Director", "vasiliev@mailbox.com", "892312398", 50000, 45);
 
         // Выводим информацию о каждом сотруднике
-        for (Employee employee : employees) {
+        for (Сотрудник employee : employees) {
             employee.displayInfo();
         }
     }
